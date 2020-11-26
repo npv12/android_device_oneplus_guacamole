66ecec5d12b0f0d919203dbcbf0959ca7883c7cd
Pranav Santhosh <59979143+npv12@users.noreply.github.com>
2020-11-26 09:00:10 +0530

    Add maintainers Tag

--- xtended_guacamole.mk
+++ xtended_guacamole.mk
@@ -17,6 +17,9 @@ $(call inherit-product, vendor/xtended/config/common_full_phone.mk)
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1440
 
+# Maintainer
+XTENDED_MAINTAINER := Pranav10121 
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := xtended_guacamole
 PRODUCT_DEVICE := guacamole
